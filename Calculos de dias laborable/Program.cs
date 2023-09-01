9fce66eab479fab3fe7dad2b6e1f1a8b0933ac93
Juan C. Mateo Peña <juancarlosmateopena@outlook.com>
2023-09-01 09:01:07 -0400

    Segundo cambios

--- Calculos de dias laborable/Program.cs
+++ Calculos de dias laborable/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Calculos_de_dias_laborable
+namespace CalculadoraDiasYsueldo
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Calculadora de Días Trabajados y Sueldo");
+
+            Console.Write("Ingrese la fecha de inicio (yyyy-MM-dd): ");
+            DateTime fechaInicio = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la fecha actual (yyyy-MM-dd): ");
+            DateTime fechaActual = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el sueldo por día: ");
+            double sueldoPorDia = Convert.ToDouble(Console.ReadLine());
+
+            // Cálculo de días trabajados
+            TimeSpan diasTrabajados = fechaActual - fechaInicio;
+            int diasLaborados = diasTrabajados.Days;
+
+            // Cálculo del sueldo
+            double sueldoTotal = sueldoPorDia * diasLaborados;
+
+            Console.WriteLine($"Días trabajados: {diasLaborados}");
+            Console.WriteLine($"Sueldo total: {sueldoTotal:C}");
+
+            Console.ReadLine();
         }
     }
 }
